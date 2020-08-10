42085ea0e03ecbe63ab2382ee5a51e93596b4fbc
hyerin6 <hyerinn6@gmail.com>
2020-08-10 21:30:53 +0900

    feat: 게시글 찜(좋아요) 도메인 구현

---
+++ src/main/java/com/depromeet/buzz/post/domain/Wish.java
@@ -0,0 +1,33 @@
+package com.depromeet.buzz.post.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import com.depromeet.buzz.user.domain.User;
+
+@Entity
+public class Wish {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private User user;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Post post;
+
+	private Wish() {
+	}
+
+	public Wish(User user, Post post) {
+		this.user = user;
+		this.post = post;
+	}
+
+}
