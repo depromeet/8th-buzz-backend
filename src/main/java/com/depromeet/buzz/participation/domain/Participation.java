155b0acb090c0496e3c0b3bed5a6e9fa80ff9dcc
hyerin6 <hyerinn6@gmail.com>
2020-08-11 23:27:17 +0900

    feat: 구매 참여 엔티티 구현

---
+++ src/main/java/com/depromeet/buzz/participation/domain/Participation.java
@@ -0,0 +1,64 @@
+package com.depromeet.buzz.participation.domain;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.depromeet.buzz.post.domain.Post;
+import com.depromeet.buzz.user.domain.User;
+
+@Entity
+public class Participation {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "post_id")
+	private Post post;
+
+	public Post getPost() {
+		return post;
+	}
+
+	private Participation() {}
+
+	public Participation(User user, Post post) {
+		this.user = user;
+		this.post = post;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Participation that = (Participation)o;
+		return Objects.equals(id, that.id);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public String toString() {
+		return "Participation{" +
+			"id=" + id +
+			", user=" + user +
+			", post=" + post +
+			'}';
+	}
+
+}
