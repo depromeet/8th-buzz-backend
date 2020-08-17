90e8a540a27adb90bb5de5a1a0a780cff020138f
SunHo Van <vsh123@naver.com>
2020-08-17 22:40:52 +0900

    feat: 한번에 응답해주는 mock 생성

--- src/main/java/com/depromeet/buzz/post/controller/PostController.java
+++ src/main/java/com/depromeet/buzz/post/controller/PostController.java
@@ -1,11 +1,6 @@
 package com.depromeet.buzz.post.controller;
 
-import com.depromeet.buzz.post.dto.CommentResponses;
-import com.depromeet.buzz.post.dto.PostDescriptionResponse;
-import com.depromeet.buzz.post.dto.PostResponse;
-import com.depromeet.buzz.post.dto.PostSellerResponse;
-import com.depromeet.buzz.post.dto.PostsRequest;
-import com.sun.org.apache.xpath.internal.operations.Bool;
+import com.depromeet.buzz.post.dto.*;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
@@ -15,11 +10,7 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,10 +22,10 @@ public class PostController {
     @GetMapping
     @ApiResponse(description = "게시글의 옵션에 따른 정보를 가져온다(페이징)")
     @Parameters(value = {
-        @Parameter(name = "category", description = "카테고리 정보", in = ParameterIn.QUERY),
-        @Parameter(name = "page", description = "현재 페이지 정보 기본은 0", in = ParameterIn.QUERY),
-        @Parameter(name = "size", description = "원하는 컨텐츠 수 기본은 10", in = ParameterIn.QUERY),
-        @Parameter(name = "sortOption", description = "정렬 기준", in = ParameterIn.QUERY)
+            @Parameter(name = "category", description = "카테고리 정보", in = ParameterIn.QUERY),
+            @Parameter(name = "page", description = "현재 페이지 정보 기본은 0", in = ParameterIn.QUERY),
+            @Parameter(name = "size", description = "원하는 컨텐츠 수 기본은 10", in = ParameterIn.QUERY),
+            @Parameter(name = "sortOption", description = "정렬 기준", in = ParameterIn.QUERY)
     })
     public ResponseEntity<Page<List<PostResponse>>> get(PostsRequest request, @PageableDefault Pageable pageable) {
         List<PostResponse> responses = new ArrayList<>();
@@ -44,10 +35,19 @@ public class PostController {
         return ResponseEntity.ok(new PageImpl(responses, pageable, 100));
     }
 
+    @GetMapping("{postId}")
+    @ApiResponse(description = "상세 게시글 정보(한번에)")
+    @Parameters(value = {
+            @Parameter(name = "postId", description = "게시글 id", in = ParameterIn.PATH)
+    })
+    public ResponseEntity<PostDetailResponse> getPostDetail(@PathVariable Long postId) {
+        return ResponseEntity.ok(PostDetailResponse.mock());
+    }
+
     @GetMapping("{postId}/banner")
     @ApiResponse(description = "상세 게시글 이미지 URL(배너)")
     @Parameters(value = {
-        @Parameter(name = "postId", description = "게시글 id", in = ParameterIn.PATH)
+            @Parameter(name = "postId", description = "게시글 id", in = ParameterIn.PATH)
     })
     public ResponseEntity<String> getPostBanner(@PathVariable Long postId) {
         return ResponseEntity.ok("https://fs.jtbc.joins.com/prog/img/mig/MOBILE/PR10010297.jpg");
@@ -56,7 +56,7 @@ public class PostController {
     @GetMapping("{postId}/description")
     @ApiResponse(description = "제품명 및 할인률, 가격")
     @Parameters(value = {
-        @Parameter(name = "postId", description = "게시글 id", in = ParameterIn.PATH)
+            @Parameter(name = "postId", description = "게시글 id", in = ParameterIn.PATH)
     })
     public ResponseEntity<PostDescriptionResponse> getPostDescription(@PathVariable Long postId) {
         return ResponseEntity.ok(PostDescriptionResponse.mock());
@@ -65,7 +65,7 @@ public class PostController {
     @GetMapping("{postId}/seller")
     @ApiResponse(description = "주최자 정보")
     @Parameters(value = {
-        @Parameter(name = "postId", description = "게시글 id", in = ParameterIn.PATH)
+            @Parameter(name = "postId", description = "게시글 id", in = ParameterIn.PATH)
     })
     public ResponseEntity<PostSellerResponse> getSeller(@PathVariable Long postId) {
         return ResponseEntity.ok(PostSellerResponse.mock());
@@ -73,9 +73,9 @@ public class PostController {
 
 
     @GetMapping("{postId}/content")
-    @ApiResponse(description = "상세 게시글 이미지 URL(배너)")
+    @ApiResponse(description = "상세 게시글 설명")
     @Parameters(value = {
-        @Parameter(name = "postId", description = "게시글 id", in = ParameterIn.PATH)
+            @Parameter(name = "postId", description = "게시글 id", in = ParameterIn.PATH)
     })
     public ResponseEntity<String> getContent(@PathVariable Long postId) {
         return ResponseEntity.ok("https://files.slack.com/files-pri/T01753WJ8H1-F01916BREKE/image.png");
@@ -85,7 +85,7 @@ public class PostController {
     @GetMapping("{postId}/comments")
     @ApiResponse(description = "제품에 대한 댓글리스트 가져옴")
     @Parameters(value = {
-        @Parameter(name = "postId", description = "게시글 id", in = ParameterIn.PATH)
+            @Parameter(name = "postId", description = "게시글 id", in = ParameterIn.PATH)
     })
     public ResponseEntity<CommentResponses> getComments(@PathVariable Long postId) {
         return ResponseEntity.ok(CommentResponses.mock());
@@ -94,7 +94,7 @@ public class PostController {
     @PostMapping("{postId}/like")
     @ApiResponse(description = "제품에 대한 좋아요 토글")
     @Parameters(value = {
-        @Parameter(name = "postId", description = "게시글 id", in = ParameterIn.PATH)
+            @Parameter(name = "postId", description = "게시글 id", in = ParameterIn.PATH)
     })
     public ResponseEntity<Boolean> like(@PathVariable Long postId) {
         return ResponseEntity.ok(true);
--- src/main/java/com/depromeet/buzz/post/dto/PostDetailResponse.java
+++ src/main/java/com/depromeet/buzz/post/dto/PostDetailResponse.java
@@ -1,16 +1,70 @@
 package com.depromeet.buzz.post.dto;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class PostDetailResponse {
-    private String name;
+    private Long postId;
+    //맨위 배너 url
+    private String BannerUrl;
+    private PostDescriptionResponse description;
+    private PostSellerResponse seller;
+    private String contentUrl;
+    private List<CommentResponse> comments;
 
     private PostDetailResponse() {
     }
 
-    public PostDetailResponse(String name) {
-        this.name = name;
+    public PostDetailResponse(Long postId,
+                              String bannerUrl,
+                              PostDescriptionResponse description,
+                              PostSellerResponse seller,
+                              String contentUrl,
+                              List<CommentResponse> comments) {
+        this.postId = postId;
+        this.BannerUrl = bannerUrl;
+        this.description = description;
+        this.seller = seller;
+        this.contentUrl = contentUrl;
+        this.comments = comments;
     }
 
-    public String getName() {
-        return name;
+    public static PostDetailResponse mock() {
+        return new PostDetailResponse(1L,
+                "https://fs.jtbc.joins.com/prog/img/mig/MOBILE/PR10010297.jpg",
+                PostDescriptionResponse.mock(),
+                PostSellerResponse.mock(),
+                "https://files.slack.com/files-pri/T01753WJ8H1-F01916BREKE/image.png",
+                Arrays.asList(
+                        CommentResponse.mock(1L, 150, "첫번째 댓글 ", true, new ArrayList<>()),
+                        CommentResponse.mock(2L, 120, "두번째 댓글 ", false, new ArrayList<>()),
+                        CommentResponse.mock(3L, 100, "세번째 댓글 ", false, new ArrayList<>())
+                ));
     }
+
+    public Long getPostId() {
+        return postId;
+    }
+
+    public String getBannerUrl() {
+        return BannerUrl;
+    }
+
+    public PostDescriptionResponse getDescription() {
+        return description;
+    }
+
+    public PostSellerResponse getSeller() {
+        return seller;
+    }
+
+    public String getContentUrl() {
+        return contentUrl;
+    }
+
+    public List<CommentResponse> getComments() {
+        return comments;
+    }
+
 }
